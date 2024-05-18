87035b982a9e703a52a797676c531e89c77a15c1
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2024-05-19 01:05:55 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <div className='px-3 py-4 h-full fixed lg:w-[20%] hidden lg:block navbar-2'>
-      <Link to='/Doogle' className=' flex items-center p-2 gap-3 rounded-[1.3rem] home text-white cursor-pointer text-center'>
+      <Link to='/' className=' flex items-center p-2 gap-3 rounded-[1.3rem] home text-white cursor-pointer text-center'>
         <div className='p-2 rounded-[1.3rem] header'><GiDogHouse size={20}/></div>
         <p className='text-center mx-auto font-bold nav'>Home</p>
       </Link>
